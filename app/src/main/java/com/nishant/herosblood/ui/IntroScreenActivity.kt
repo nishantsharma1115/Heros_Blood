80a2b5fecb5edcf5e2e0f192eafbc06f344e88ea
Nishant Sharma <64049931+nishantsharma1115@users.noreply.github.com>
2020-12-23 19:58:22 +0530

    Connect to Firebase

--- app/src/main/java/com/nishant/herosblood/ui/IntroScreenActivity.kt
+++ app/src/main/java/com/nishant/herosblood/ui/IntroScreenActivity.kt
@@ -4,12 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.nishant.herosblood.R
 import com.nishant.herosblood.databinding.ActivityIntroScreenBinding
 
 class IntroScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityIntroScreenBinding
+    private val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+    override fun onStart() {
+        super.onStart()
+        if (user != null) {
+            startActivity(Intent(this, UserDashboardActivity::class.java))
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/nishant/herosblood/ui/UserDashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.nishant.herosblood.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nishant.herosblood.R
+
+class UserDashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_dashboard)
+    }
+}
