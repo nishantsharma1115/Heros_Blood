c789896b50899e4ae8ef41d5b9b9b695ce081bcb
Nishant Sharma <64049931+nishantsharma1115@users.noreply.github.com>
2021-02-05 14:56:19 +0530

    Splash Screen Implemented

--- app/src/main/java/com/nishant/herosblood/ui/IntroScreenActivity.kt
+++ app/src/main/java/com/nishant/herosblood/ui/IntroScreenActivity.kt
@@ -24,6 +24,7 @@ class IntroScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.FullScreenTheme)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro_screen)
 
         binding.btnLogin.setOnClickListener {
