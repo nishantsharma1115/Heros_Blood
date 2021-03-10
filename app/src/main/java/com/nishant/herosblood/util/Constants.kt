efdab3b135106bcdef5c3c50a5b7e08fbf7b4f59
Nishant <official.nishant1115@gmail.com>
2021-03-10 09:33:09 +0530

    screenshots added

---
+++ app/src/main/java/com/nishant/herosblood/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.nishant.herosblood.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "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"
+        const val BASE_URL = "https://api.sendgrid.com/v3/"
+        const val COMPANY_MAIL = "heroesblood02@gmail.com"
+        const val COMPANY_NAME = "Heroes Blood"
+        const val MAP_API_KEY = "AIzaSyCWKzn7QT7T_fuUckPumvh4bHOy2R3Ztvs"
+    }
+}
