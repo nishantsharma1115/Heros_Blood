73a8a170b5912adf58e73e29bea72639687d95fc
Nishant Sharma <64049931+nishantsharma1115@users.noreply.github.com>
2021-09-25 12:14:09 +0530

    Design changes to Login Sign up and Intro Screen.

--- app/src/main/java/com/nishant/herosblood/adapters/BindingAdapter.kt
+++ app/src/main/java/com/nishant/herosblood/adapters/BindingAdapter.kt
@@ -1,12 +1,23 @@
 package com.nishant.herosblood.adapters
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.nishant.herosblood.R
 
 @BindingAdapter("setEmailWithDescription")
 fun TextView.setEmailWithDescription(email: String) {
     val desc = "Please type the verification code sent to $email"
-    this.text = desc
+    val spannable = SpannableString(desc)
+    spannable.setSpan(
+        ForegroundColorSpan(resources.getColor(R.color.redColor)),
+        42,
+        desc.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    this.text = spannable
 }
 
 @BindingAdapter("setWeight")
