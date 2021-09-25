524fe46480784723c3eabd90844299ded159fc5a
Nishant Sharma <64049931+nishantsharma1115@users.noreply.github.com>
2021-09-25 17:22:43 +0530

    Dashboard design complete..

--- app/src/main/java/com/nishant/herosblood/ui/UserDashboardActivity.kt
+++ app/src/main/java/com/nishant/herosblood/ui/UserDashboardActivity.kt
@@ -230,9 +230,9 @@ class UserDashboardActivity : AppCompatActivity() {
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
         ) {
             locationLivedata.startListening()
         } else {
